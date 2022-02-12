3e428c436e6ad104a84ec829a7da72852b9fa7e6
Saumya Singh <53351628+saumyasingh20@users.noreply.github.com>
2022-02-12 16:58:53 +0530

    set up the mvc directory structure

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 8004;
+const db = require('./config/mongoose');
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+//extract styles and scripts from the layout
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+app.use(expressLayouts);
+//middleware to parse the requests
+app.use(express.urlencoded());
+
+app.set('view engine','ejs');
+app.set('views','./views');
+//use express router
+app.use('/',require('./routes'));
 
 app.listen(port,function(err){
     if(err){
